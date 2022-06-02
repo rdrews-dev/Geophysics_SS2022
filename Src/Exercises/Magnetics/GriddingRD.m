4a007c98e5b4460f6ecbe9359460da23830eb826
rdrews-dev <51914390+rdrews-dev@users.noreply.github.com>
2022-06-02 14:12:29 +0200

    worked more
    just survived

--- Src/Exercises/Magnetics/GriddingRD.m
+++ Src/Exercises/Magnetics/GriddingRD.m
@@ -9,7 +9,7 @@ x = data(:,3);y=data(:,4);z=data(:,5);
 xlin = linspace(min(x), max(x), 100);
 ylin = linspace(min(y), max(y), 100);
 [X,Y] = meshgrid(xlin, ylin);
-Z = griddata(x,y,z,X,Y,'natural');
+Z = griddata(x,y,z,X,Y);
 mesh(X,Y,Z)
 axis tight; hold on
 plot3(x,y,z,'.','MarkerSize',15)
